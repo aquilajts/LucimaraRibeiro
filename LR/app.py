2d8c50e7431873d86ff344d8467751a007ce6877
aquilajts <aquilajts@gmail.com>
2025-10-02 13:22:12 -0300

    Update app.py

--- LR/app.py
+++ LR/app.py
@@ -160,6 +160,5 @@ def agendamento():
 # Execução
 # -------------------------
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
